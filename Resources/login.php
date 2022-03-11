4864be9554ff9aeea878b7e713ca6bd7c8bb1add
Ishaan <ishaansharma1975@gmail.com>
2022-03-11 23:14:35 +0530

    Updated Login

--- Resources/login.php
+++ Resources/login.php
@@ -29,7 +29,7 @@ and password='".md5($password)."'";
     }else{
 ?>
  <form class="login" action="" method="post" name="login">
-    <h1 class="login-title">Login</h1>
+    <h1 class="login-title">Login Here Please</h1>
     <input type="text" class="login-input" name="username" placeholder="Username" autofocus>
     <input type="password" class="login-input" name="password" placeholder="Password">
     <input type="submit" value="Login" name="submit" class="login-button">
