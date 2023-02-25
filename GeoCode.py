3826c8ab44203e2ced92d5fec773e40edace19e4
IshaanS <ishaansharma1975@gmail.com>
2023-02-25 18:17:40 +0530

    updated Geo Fetch

--- GeoCode.py
+++ GeoCode.py
@@ -1,19 +1,18 @@
 #Program to Input Geo Location 
-
+import requests
 geo_lat = float(input("Enter you lattitude: "))
 geo_lon = float(input("Enter your longitude: "))
-
-
-
-import requests
 r = requests.get('https://get.geojs.io/')
 ip_request = requests.get('https://get.geojs.io/v1/ip.json')
-ipAdd = ip_request.json()['ip']
-print(ipAdd)
-url = 'https://get.geojs.io/v1/ip/geo/'+ipAdd+'.json'
+ip = ip_request.json()['ip']
+print(ip)
+url = 'https://get.geojs.io/v1/ip/geo/'+ip+'.json'
 geo_req = requests.get(url)
 geo_data = geo_req.json()
 longitude_fetch = geo_data.get('longitude')
 latitude_fetch = geo_data.get('latitude')
-city_fetch = latitude_fetch = geo_data.get('City')
+city_fetch = geo_data.get('city')
+print(longitude_fetch)
+print(latitude_fetch)
+print(city_fetch)
 
