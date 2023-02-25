c1de5d8a14c1382326fa5dd8e47e65e60ce392ce
IshaanS <ishaansharma1975@gmail.com>
2023-02-25 21:02:38 +0530

    Updated Program

--- DAC_Generator.py
+++ DAC_Generator.py
@@ -73,25 +73,28 @@ def STL(string):
     
     
 g=STL(dac1)
-m=0
-for i in range(0,5):
-    if ord(g[i])<48 or ord(g[i])>57:
-        m=1
-        break
-    else:
-        continue
-def platlon(latitude,longitude):
-    print(f"Latitude is {latitude}")
-    print(f"Longitude is {longitude}")
-if m==0:
-    lat2=(int(g[1])*100)+(int(g[2])*10)+int(g[0])
-    lon2=(int(g[5])*100)+(int(g[3])*10)+int(g[4])
-    if lat2%9==0 or lon2%5==0:
-        print("The address is real")
-        #latt=(lat2/9) + ((ord(g[7])-65)*0.1) + ((77-ord(g[8]))*0.01) + ((ord(g[9])-77)*0.001) + ((90-ord(g[6]))*0.0001)
-        #lonn=(lon2/5) + ((ord(g[11])-65)*0.1) + ((77-ord(g[12]))*0.01) + ((ord(g[13])-77)*0.001) + ((90-ord(g[10]))*0.0001)
-        #platlon(latt,lonn)
-        
+if len(g)==14:
+    m=0
+    for i in range(0,5):
+        if ord(g[i])<48 or ord(g[i])>57:
+            m=1
+            break
+        else:
+            continue
+    def platlon(latitude,longitude):
+        print(f"Latitude is {latitude}")
+        print(f"Longitude is {longitude}")
+    if m==0:
+        lat2=(int(g[1])*100)+(int(g[2])*10)+int(g[0])
+        lon2=(int(g[5])*100)+(int(g[3])*10)+int(g[4])
+        if lat2%9==0 or lon2%5==0:
+            print("The address is real")
+            #latt=(lat2/9) + ((ord(g[7])-65)*0.1) + ((77-ord(g[8]))*0.01) + ((ord(g[9])-77)*0.001) + ((90-ord(g[6]))*0.0001)
+            #lonn=(lon2/5) + ((ord(g[11])-65)*0.1) + ((77-ord(g[12]))*0.01) + ((ord(g[13])-77)*0.001) + ((90-ord(g[10]))*0.0001)
+            #platlon(latt,lonn)
+            
+        else:
+            print("The given DAC is fake.")
     else:
         print("The given DAC is fake.")
 else:
